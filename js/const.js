c20db94011b7d62f0223e7d28637a0bf1a7b39cb
Daniel Pantea <dani.pantea@gmail.com>
2022-09-12 14:16:27 +0300

    update 12sep

---
+++ js/const.js
@@ -0,0 +1,5 @@
+const inbox = 100;  // A constant needs to have an initial value when it is declared
+
+// inbox = 50; --> A constant cannot be changed like a variable 
+
+console.log(inbox);
---
+++ js/variables.js
@@ -0,0 +1,20 @@
+// let inbox = 100;
+// alert(inbox);
+// alert(inbox + 1);
+
+// 
+
+// let inbox1 = 100;
+// let inbox2 = 256;
+// let allEmails = inbox1 + inbox2;
+// alert(allEmails);
+
+// 
+
+// let inbox = 100;
+// inbox = 250;
+// alert(inbox);
+
+let inbox = 100;
+console.log("Afisare:")
+console.log(inbox);
