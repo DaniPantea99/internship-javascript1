33f459595983ec4e7b1dac7d01de28ffe0699aff
Daniel Pantea <dani.pantea@gmail.com>
2022-09-12 17:26:45 +0300

    update2 12sep

---
+++ js/dataTypes.js
@@ -0,0 +1,15 @@
+// Primitive Data Types
+
+// Numbers - 1 2 3
+
+// Strings - dani 
+
+// Booleans - true | false
+
+// Undefined - let name;
+
+// Null - let name = null;
+ 
+// Symbol - const name = Symbol();
+
+// OBJECT (!!!!!)
--- js/operators.js
+++ js/operators.js
@@ -2,3 +2,50 @@
 
 // Basic operators
 
+let inbox = 10;
+
+console.log(inbox + 10);  // Addition
+console.log(inbox - 5);  // Subtraction
+console.log(inbox * 3);  // Multiplication
+console.log(inbox / 10);  // Division
+
+
+//Exponentiation Operator
+console.log("Exponentiation:")
+console.log(inbox ** 2); // produces the same result as Math.pow(x, y)
+
+// let x=5;
+// let z=Math.pow(x,2);
+
+//Exponentiation Assignment
+
+// let x=5;
+// x **=2; 
+
+
+// Modulus (Division Remainder)
+
+console.log("Modulus:");
+console.log(inbox % 3);
+
+
+
+// Increment
+
+console.log("Increment:")
+inbox++;
+console.log(inbox);
+
+
+// 	Decrement
+
+console.log("Decrement:")
+inbox--;
+console.log(inbox);
+
+
+inbox += 10;
+console.log(inbox);
+
+
+
