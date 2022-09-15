32d4ed68d5da192242d2df270148c883a0ccac61
Daniel Pantea <dani.pantea@gmail.com>
2022-09-15 14:58:41 +0300

    udapte 15sep

--- todo-list/app.js
+++ todo-list/app.js
@@ -48,7 +48,7 @@ function addTodo(event) {
 function deleteCheck(e) {
     // console.log(e.target);
 
-    const item = e.target;
+    const item = e.target; 
 
     //Delete todo
     if (item.classList[0] === "trash-btn") {
